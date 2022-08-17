3f80ddface68984877b8d5e86c985267f08a4faf
agvan <agvandyke25@gmail.com>
2022-08-17 14:27:13 -0500

    finished routes and fixed pizza model

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,35 +1,23 @@
-//imports the Schema constructor from the mongoose library
-const { Schema, model } = require("mongoose")
+const { Schema, model } = require('mongoose');
 
-//schema for the Pizza model
-
-/*Contains:
-Name of the pizza, name of the user that created the pizza,
-timestamp of when the pizza was created, timestamp of any updates of the pizza's data, 
-pizza's suggested size, & pizza's toppings. */
-
-const PizzaSchema = newSchema({
-    pizzaName: {
-        type: String
-    },
-    createdBy: {
-        type: String
-    },
-    createAt: {
-        type: Date,
-        default: Date.now
-    },
-    size: {
-        type: String,
-        default: 'Large'
-    },
-    toppings: []
+const PizzaSchema = new Schema({
+  pizzaName: {
+    type: String
+  },
+  createdBy: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  size: {
+    type: String,
+    default: 'Large'
+  },
+  toppings: []
 });
-
-//Pizza model
 
 const Pizza = model('Pizza', PizzaSchema);
 
-//exports the Pizza model
 module.exports = Pizza;
-
