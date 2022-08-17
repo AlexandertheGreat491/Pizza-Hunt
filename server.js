63baf1087bed36fe040862fbe921b78d9c80f4d7
agvan <agvandyke25@gmail.com>
2022-08-17 13:56:05 -0500

    fixed issue with mongoose connection, added routes and controllers

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hunt', {
-    userNewUrlParser: true,
-    useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true 
+}); 
 
 //logs mongo queries being executed
 mongoose.set('debug', true);
