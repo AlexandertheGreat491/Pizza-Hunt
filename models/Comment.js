4e8fa44ac969788a8ec4a3891fd3df902f68c9b2
agvan <agvandyke25@gmail.com>
2022-08-22 11:36:34 -0500

    created the Reply schema & added comments

--- models/Comment.js
+++ models/Comment.js
@@ -1,5 +1,23 @@
 const { Schema, model } = require('mongoose');
 
+// Reply schema
+const ReplySchema = new Schema(
+  {
+    replyBody: {
+      type: String
+    },
+    writtenBy: {
+      type: String
+    },
+    createdAt: {
+      type: Data,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
+  }
+);
+
+// Comment schema
 const CommentSchema = new Schema({
   writtenBy: {
     type: String
