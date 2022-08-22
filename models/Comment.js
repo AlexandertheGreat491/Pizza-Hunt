996a82e8e94a900ffde8d30e527631644949ce84
agvan <agvandyke25@gmail.com>
2022-08-22 11:59:13 -0500

    added statement to import dateFormat(), added replyId to make it distinct from parent comment _id, updated createdAt fields to timestamp comments & replies, and added comments

--- models/Comment.js
+++ models/Comment.js
@@ -1,8 +1,18 @@
 // import code statement
 const { Schema, model, Types } = require('mongoose');
 
+// imports the dateFormat() function
+const dateFormat = require('../utils/dateFormat');
+
 // Reply schema
 const ReplySchema = new Schema(
+  {
+    // sets a custom id to prevent confusion with the parent comment _id
+    replyId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    }
+  },
   {
     replyBody: {
       type: String
@@ -10,6 +20,7 @@ const ReplySchema = new Schema(
     writtenBy: {
       type: String
     },
+    //creates timestamps for comments
     createdAt: {
       type: Data,
       default: Date.now,
@@ -26,10 +37,14 @@ const CommentSchema = new Schema({
   commentBody: {
     type: String
   },
+  // creates timestamps for comments
   createdAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+    get: createdAtVal => dateFormat(createdAtVal)
+  },
+  // populates an array of data that follows the ReplySchema definition
+  replies: [ReplySchema]
 });
 
 const Comment = model('Comment', CommentSchema);
