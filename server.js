109786f1dd7e3c9cc97ba47baf1df52c9bbbe895
agvan <agvandyke25@gmail.com>
2022-08-22 15:30:14 -0500

    fixed code

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use(require('./routes'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
   useNewUrlParser: true,
@@ -18,5 +17,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hun
 //logs mongo queries being executed
 mongoose.set('debug', true);
 
+app.use(require('./routes'));
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
